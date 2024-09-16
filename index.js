db9d469038bf63cd47f684335f522bf6dbf6aee5
Blake Lucey <blucey7@gmail.com>
2024-09-16 12:40:06 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// Import required libraries
+const express = require("express");
+const { initializeDB } = require("./helpers/dynamoDB");
+const contentRoute = require("./routes/content");
---
+++ routes/content.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const AWS = require("aws-sdk");
+const redis = require("redis");
+
+
